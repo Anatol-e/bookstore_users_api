ace70ef190a21535da262232d5b6297a8e9d8286
Anatol-e <bolshoymedvedtoliano@gmail.com>
2020-08-24 23:06:04 +0300

    Simple validation

--- domain/users/user.go
+++ domain/users/user.go
@@ -1,9 +1,22 @@
 package users
 
+import (
+	"github.com/Anatol-e/bookstore_users_api/utils/errors"
+	"strings"
+)
+
 type User struct {
 	Id          int64  `json:"id"`
 	FirstName   string `json:"firstname"`
 	LastName    string `json:"lastname"`
 	Email       string `json:"email"`
 	DateCreated string `json:"date_created"`
+}
+
+func (user *User) Validate() *errors.RestErr {
+	user.Email = strings.TrimSpace(strings.ToLower(user.Email))
+	if user.Email == "" {
+		return errors.NewBadRequestError("invalid email address")
+	}
+	return nil
 }
