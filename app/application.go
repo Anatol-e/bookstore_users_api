a96f1a6602f5e1157e4fd8514c40b0cdebdd153f
Anatol-e <bolshoymedvedtoliano@gmail.com>
2020-08-27 10:19:57 +0300

    Logger

--- app/application.go
+++ app/application.go
@@ -1,10 +1,14 @@
 package app
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/Anatol-e/bookstore_users_api/logger"
+	"github.com/gin-gonic/gin"
+)
 
 var router = gin.Default()
 
 func StartApplication() {
 	mapUrl()
+	logger.Info("about to start the application")
 	router.Run(":8080")
 }
